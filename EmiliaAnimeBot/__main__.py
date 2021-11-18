fe87bf71afd4554b4d50b57b0472b26782d84bb5
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-18 20:18:27 +0700

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -78,9 +78,9 @@ buttons = [
         ),
     ],    
     [
-       InlineKeyboardButton(text="•• sᴜᴘᴘᴏʀᴛ ••", url=f"https://t.me/{SUPPORT_CHAT}"
-       InlineKeyboardButton(
-          text="•• sᴏᴜʀᴄᴇ ••", url="https://www.xnxx.com" # If you have a bit of dignity left in you, Do NOT Remove this Button
+        InlineKeyboardButton(text="•• sᴜᴘᴘᴏʀᴛ ••", url=f"https://t.me/{SUPPORT_CHAT}"
+        InlineKeyboardButton(
+            text="•• sᴏᴜʀᴄᴇ ••", url="https://www.xnxx.com" # If you have a bit of dignity left in you, Do NOT Remove this Button
         ),
      
     ],
